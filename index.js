31d64b77429927d1531d4ba02cb29fb86ecf7c67
Valentyn Kovalskyi <kovalsky.valentine@gmail.com>
2025-03-21 19:50:25 +0200

    Start develop base Vue app structure

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+import chalk from "chalk";
 import prompts from "prompts";
 
 const result = await prompts([
@@ -16,12 +18,30 @@ const result = await prompts([
         ]
     },
     {
-        name: "addVuetify",
-        type: "select",
-        message: "What is your sex?",
+        name: "vuePlugins",
+        type: "multiselect",
+        message: "Choose Vue plugins",
         choices: [
-            { title: "Yes", value: true},
-            { title: "No", value: false}
+            { title: "Vue-Router", value: "router"},
+            { title: "Pinia State Manager", value: "pinia"},
+            { title: "Vuetify UI", value: "vuetify"},
+            { title: "I18n", value: "i18n"},
+        ]
+    },
+    {
+        name: "expressPlugins",
+        type: "multiselect",
+        message: "Choose Vue plugins",
+        choices: [
+            { title: "Helmet", value: "helmet"},
+            { title: "Morgan Logger", value: "morgan"},
+            { title: "Compression", value: "compression"},
         ]
     }
 ])
+
+
+
+
+
+console.log(chalk.greenBright.bold(JSON.stringify(result)))
---
+++ templates/frontend/src/components/Message.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>{{ messageText }}</h1>
+</template>
+<script setup>
+defineProps([
+    'messageText'
+])
+</script>
